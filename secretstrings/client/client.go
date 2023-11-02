df8274cf85812b922747787f280b5c6db29b19f4
Matthew Cudby <oh22896@bristol.ac.uk>
2023-11-02 20:36:04 +0000

    first commit

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,42 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
+	"net/rpc"
+	"os"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
-	//TODO: connect to the RPC server and send the request(s)
+
+	//Need to connect to RPC server and send the request
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	/*request := stubs.Request{Message: "Hello"}
+	response := new(stubs.Response)
+
+	client.Call(stubs.PremiumReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)*/
+
+	//Read word from list
+	//Send them off to client to be reversed
+
+	file, _ := os.Open("wordlist")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		//fmt.Println(text)
+		request := stubs.Request{Message: text}
+		response := new(stubs.Response)
+
+		//Call() waits for it's completion
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println("Responded: " + response.Message)
+	}
 }
--- secretstrings/server/server.go
+++ secretstrings/server/server.go
@@ -1,23 +1,50 @@
-
 package main
+
 import (
-//	"errors"
-//	"flag"
-//	"fmt"
-//	"net"
-	"time"
+	"flag"
 	"math/rand"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
-//	"net/rpc"
+	"net"
+	"net/rpc"
+	"time"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-/** Super-Secret `reversing a string' method we can't allow clients to see. **/
 func ReverseString(s string, i int) string {
-    time.Sleep(time.Duration(rand.Intn(i))* time.Second)
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	time.Sleep(time.Duration(rand.Intn(i)) * time.Second)
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
+type SecretStringOperations struct{}
+
+func (s *SecretStringOperations) Reverse(req stubs.Request, res *stubs.Response) (err error) {
+	res.Message = ReverseString(req.Message, 10)
+	return
+}
+
+func (s *SecretStringOperations) FastReverse(req stubs.Response, res *stubs.Response) (err error) {
+	res.Message = ReverseString(req.Message, 2)
+	return
+}
+func main() {
+	//Adds optional flag with default value
+	pAddr := flag.String("port", "8030", "Port to listen to")
+	flag.Parse()
+
+	//Initialising random
+	rand.Seed(time.Now().UnixNano())
+
+	//Register the SecretStringOperations struct and it's methods with rpc
+	rpc.Register(&SecretStringOperations{})
+
+	//Listen() function creates servers
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
+
+	defer listener.Close()
+	//Accept accepts connections on the listener
+	rpc.Accept(listener)
+
+}
