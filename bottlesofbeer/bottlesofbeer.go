03ec2b2ee4e1e68981fe24d01743385d05c9b72e
Matthew Cudby <oh22896@bristol.ac.uk>
2023-11-03 18:34:03 +0000

    edit

--- bottlesofbeer/bottlesofbeer.go
+++ bottlesofbeer/bottlesofbeer.go
@@ -65,7 +65,7 @@ func main() {
 			fmt.Println("Listening on Listener...")
 			rpc.Accept(listener)
 		}
-		wg.Done()
+		wg.Done()Dial
 		return
 	}()
 
@@ -83,7 +83,9 @@ func main() {
 		err = client.Call("Operations.CallNextInChain", request, response)
 		fmt.Println("Attempted RPC...")
 		if err != nil {
+
 			log.Fatal("Error with Call()")
+			fmt.Println(err)
 		}
 
 		if response.finished {
